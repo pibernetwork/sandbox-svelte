9a5db9f6b2f7d3901f22bc93aeba2f8bc8e31def
Mauricio Piber <mauriciopiber@gmail.com>
2023-09-04 14:23:40 -0300

    Save tests package

---
+++ packages/tests/src/lib/components/MyComponent.test.ts
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/svelte";
+import { describe, expect, test } from "vitest";
+import MyComponent from "./MyComponent.svelte";
+
+describe("Todo", () => {
+  test("shows the todo text when rendered", () => {
+    render(MyComponent, { props: {} });
+
+    expect(screen.getByText("My Component")).toBeDefined(); // checkbox
+  });
+});
