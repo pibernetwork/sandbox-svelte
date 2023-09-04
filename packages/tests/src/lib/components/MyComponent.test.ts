3a542efbf9407b29dd716e1af7f25c4e7480b03d
Mauricio Piber <mauriciopiber@gmail.com>
2023-09-04 14:26:50 -0300

    Added tailwindcss

--- packages/tests/src/lib/components/MyComponent.test.ts
+++ packages/tests/src/lib/components/MyComponent.test.ts
@@ -1,11 +1,11 @@
-import { render, screen } from "@testing-library/svelte";
-import { describe, expect, test } from "vitest";
-import MyComponent from "./MyComponent.svelte";
+import { render, screen } from '@testing-library/svelte';
+import { describe, expect, test } from 'vitest';
+import MyComponent from './MyComponent.svelte';
 
-describe("Todo", () => {
-  test("shows the todo text when rendered", () => {
+describe('Todo', () => {
+  test('shows the todo text when rendered', () => {
     render(MyComponent, { props: {} });
 
-    expect(screen.getByText("My Component")).toBeDefined(); // checkbox
+    expect(screen.getByText('My Component')).toBeDefined(); // checkbox
   });
 });
