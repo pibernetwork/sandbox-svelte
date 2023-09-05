a98f213d218d2a73c76aaba67d64b0812a565d5b
Mauricio Piber <mauriciopiber@gmail.com>
2023-09-05 11:44:31 -0300

    Added typeof components

--- packages/components/src/lib/components/Data/Data.svelte
+++ packages/components/src/lib/components/Data/Data.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
   import type { ModeEventProps } from '$lib/actions/mode';
-  import type { SelectEventProps } from '$lib/actions/selected';
   import type { SetStateProps } from '$lib/actions/state';
   import type { PageMode } from '$lib/types';
   import { Modal } from 'flowbite-svelte';
-  import type { ComponentType } from 'svelte';
-  import PageHeader from '../Layout/PageHeader.svelte';
 
-  export let Table: ComponentType;
-  export let Filter: ComponentType;
-  export let Form: ComponentType;
-  export let Delete: ComponentType;
-  export let View: ComponentType;
+  import PageHeader from '$lib/components/Layout/PageHeader.svelte';
+  import type DeleteWrapper from '$lib/components/Sandbox/Delete.svelte';
+  import type FilterWrapper from '$lib/components/Sandbox/Filter.svelte';
+  import type FormWrapper from '$lib/components/Sandbox/Form.svelte';
+  import type TableWrapper from '$lib/components/Sandbox/Table.svelte';
+  import type ViewWrapper from '$lib/components/Sandbox/View.svelte';
+
+  export let Table: typeof TableWrapper;
+  export let Filter: typeof FilterWrapper;
+  export let Form: typeof FormWrapper;
+  export let Delete: typeof DeleteWrapper;
+  export let View: typeof ViewWrapper;
 
   export let title: string;
 
@@ -35,26 +39,17 @@
     }
   }
 
-  function setSelected(event: CustomEvent<SelectEventProps>) {
-    selected = event.detail.selected;
-  }
-
   $: selected = mode === null ? null : selected;
 </script>
 
-<PageHeader on:setMode={setMode} on:setSelected={setSelected} {title} on:setState={setState} />
-<svelte:component
-  this={Table}
-  on:setMode={setMode}
-  on:setSelected={setSelected}
-  on:setState={setState}
-/>
+<PageHeader on:setMode={setMode} {title} on:setState={setState} />
+<svelte:component this={Table} on:setMode={setMode} on:setState={setState} />
 <Modal bind:open={isOpen} outsideclose>
   {#if mode === 'filters'}
     <svelte:component this={Filter} on:setMode={setMode} on:setState={setState} />
   {/if}
   {#if mode === 'create'}
-    <svelte:component this={Form} on:setMode={setMode} on:setState={setState} />
+    <svelte:component this={Form} on:setMode={setMode} on:setState={setState} selected={null} />
   {/if}
   {#if mode === 'view'}
     <svelte:component this={View} on:setMode={setMode} {selected} on:setState={setState} />
